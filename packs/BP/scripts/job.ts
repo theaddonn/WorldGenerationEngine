55273c89e818162d2a624e17c740a78e2fd77921
theaddon <theaddonn@gmail.com>
2024-10-16 23:11:24 +0200

    prettier

--- packs/BP/scripts/job.ts
+++ packs/BP/scripts/job.ts
@@ -2,26 +2,24 @@ import { system, world } from "@minecraft/server";
 
 const job_ids: (number | null)[] = [];
 
-
 export function runJob(e: Generator<void, void, void>) {
     job_ids.push(system.runJob(e));
 }
 
 export function clearJobs() {
-  for (const job_id of job_ids) {
-    if (job_id == null) {
-      continue;
+    for (const job_id of job_ids) {
+        if (job_id == null) {
+            continue;
+        }
+        system.clearJob(job_id);
     }
-    system.clearJob(job_id);
-  }
 }
 
 export function stopJob(id: number) {
-
-  for (let job_id of job_ids) {
-    if (job_id == id) {
-      job_id = null;
-      system.clearJob(id);
+    for (let job_id of job_ids) {
+        if (job_id == id) {
+            job_id = null;
+            system.clearJob(id);
+        }
     }
-  }
 }
