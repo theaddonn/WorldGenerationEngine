b0e1f52e68038d9cbd6bc75cf2796e5ecfdfe55a
Akashic Records <76927522+akashic-records-of-the-abyss@users.noreply.github.com>
2024-10-16 21:36:50 +0100

    Removed Legacy Code

--- packs/BP/scripts/main.ts
+++ packs/BP/scripts/main.ts
@@ -4,54 +4,6 @@ import { clearJobs, runJob } from "./job";
 import { PerlinNoise3D } from "./worldgen/noise";
 import { managePlayer } from "./worldgen/generation";
 
-
-system.afterEvents.scriptEventReceive.subscribe((event) => {
-  
-  switch (event.id) {
-    
-
-    case "wge:generate":
-      clearJobsEvent();
-      generateEvent();
-      break;
-    case "wge:clear":
-      clearJobsEvent();
-      clearEvent();
-      break;
-    case "wge:clear_jobs":
-      clearJobsEvent();
-      break;
-    default:
-      if (event.id.startsWith("wge:")) {
-        world.sendMessage(`Unknown ScriptEvent ${event.id}`);
-      }
-      break;
-  }
-});
-system.beforeEvents.watchdogTerminate.subscribe((ev) => {
-  ///Do whatever
-  ev.cancel = true
-  });
-function generateEvent() {
-  runJob(
-    generate({ x: -16, y: -32, z: -16 }, { x: 16, y: 32, z: 16 }, world.getDimension("overworld"))
-  );
-}
-
-function clearEvent() {
-    runJob(cleanUp({ x: -16, y: -32, z: -16 }, { x: 16, y: 32, z: 16 }, world.getDimension("overworld")));
-}
-
-function clearJobsEvent() {
-  world.sendMessage(`[§aWGE§r] [§3INFO§r] Started cancelling jobs...`);
-  clearJobs();
-  world.sendMessage(`[§aWGE§r] [§3INFO§r] Finished cancelling jobs!`);
-}
-
-
-
-
-
 system.runInterval(() => {
   let dim = world.getDimension("overworld");
   dim.getPlayers().forEach((player) => {
