8f37f14a47aeab43fb0f1cefce548b781f17bf9c
Akashic Records <76927522+akashic-records-of-the-abyss@users.noreply.github.com>
2024-10-18 18:03:54 +0100

    Throw Util

--- packs/BP/scripts/worldgen/util.ts
+++ packs/BP/scripts/worldgen/util.ts
@@ -4,3 +4,7 @@ import { SUBCHUNK_SIZE } from "./chunk";
 export function idx2D(pos: Vector2 | Vector3) {
     return pos.x * SUBCHUNK_SIZE + pos.y;
 }
+
+export function throwError(err: Error): never {
+    throw err
+}
